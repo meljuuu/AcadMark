4e52bb361d1831599d811612dbd2fc8037ff8903
porarisuu <112392424+meljuuu@users.noreply.github.com>
2025-03-20 00:40:46 +0800

    Update header.vue

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="absolute top-0 left-0 w-full bg-blue z-0">
-    <div class="flex justify-between items-center px-5 py-5 sm:ml-28 md:ml-50 lg:ml-50 md:px-5 lg:px-15">
+    <div class="flex justify-between items-center px-5 py-2 sm:ml-28 md:ml-50 lg:ml-50 md:px-5 lg:px-15">
       <h1 class="sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-white">
         Welcome Teacher Anna!
       </h1>
